376a93fc2097a8957422fdc4f520b05a523c72e5
Oleh Subotin <87891790+olehsubotin@users.noreply.github.com>
2025-05-19 08:39:41 +0300

    feat: build app header with search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react';
+import Search from './components/Search';
 import './App.css';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <>
-      <h1 className='text-3xl font-bold underline text-blue-500'>
-        Hello world!
-      </h1>
-    </>
+    <main>
+      <div className='pattern'>
+        <div className='wrapper'>
+          <header>
+            <img src='./hero.png' alt='Hero banner' />
+            <h1>
+              Find <span className='text-gradient'>Movies</span> You'll Enjoy
+              Without the Hassle
+            </h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+            <h3 className='text-white'>{searchTerm ? searchTerm : ''}</h3>
+          </header>
+        </div>
+      </div>
+    </main>
   );
 }
 
